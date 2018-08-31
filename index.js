530fa63ad3a47711a814a3e46610d7906962a7c4
jisufeifei <2934251107@qq.com>
2018-08-31 17:30:09 +0800

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const  path = require('path');
 const server = express();
 server.get('/',(req,res) =>{
-    res.send('ok');
+    res.sendFile(
+        path.resolve('./views/index.html')
+    );
 });
 server.listen(80);
